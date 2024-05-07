04c24bdb113a3bcc9c6b83902f30c27ff9805e5e
d4xwrld <reyzcraft87@gmail.com>
2024-05-07 18:51:02 +0700

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Swal from "sweetalert2";
-import Nav from "./component/Nav";
+import Nav from "./components/Nav";
 
 function App() {
   function Checker() {
     Swal.fire("SweetAlert2 is working!");
   }
+  // Let Links =[
+  //   {name:"HOME", link:"/"},
+  //   {name:"ABOUT", link:"/"},
+  // ];
 
   return (
     <>
       <Nav />
       <div className="bg-indigo-600 w-full h-screen"></div>
+      <div className="md:flex bg-white py-4">
+        <div className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-gray-800">
+          <span className="text-3xl text-indigo-600 mr-1 pt-2"></span>
+        </div>
+      </div>
     </>
     // <div className="App">
     //   <header className="App-header">
